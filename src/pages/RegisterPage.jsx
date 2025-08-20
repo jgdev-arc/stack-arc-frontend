3afc18ceef46337eca854d6a2dfa4677e0f9cd42
Jonathan G <pcgpod@yahoo.com>
2025-08-19 21:13:19 -0500

    added config file for azure

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,85 +1,89 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ApiService from "../service/ApiService";
 
 const RegisterPage = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [message, setMessage] = useState("");
-    const [role, setRole] = useState("USER"); 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const [role, setRole] = useState("USER");
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const showMessage = (msg) => {
+    setMessage(msg);
+    setTimeout(() => {
+      setMessage("");
+    }, 4000);
+  };
 
-    const showMessage = (msg) => {
-        setMessage(msg);
-        setTimeout(() => {
-            setMessage("");
-        }, 4000);
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const registerData = { name, email, password, phoneNumber, role };
+      await ApiService.registerUser(registerData);
+      setMessage("Registration Successfull");
+      navigate("/login");
+    } catch (error) {
+      showMessage(
+        error.response?.data.message || "An error has occured" + error
+      );
+      console.log(error);
     }
+  };
 
-    const handleRegister = async(e) => {
-        e.preventDefault();
+  return (
+    <div className="auth-container">
+      <h2>Register</h2>
+      {message && <p className="message">{message}</p>}
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
-        try {
-            const registerData = {name, email, password, phoneNumber, role};
-            await ApiService.registerUser(registerData);
-            setMessage("Registration Successfull");
-            navigate("/login");
-        } catch (error) {
-            showMessage(error.response?.data.message || "An error has occured" + error);
-            console.log(error);
-        }
-    }
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-    return(
-        <div className="auth-container">
-            <h2>Register</h2>
-            {message && <p className="message">{message}</p>}
-            <form onSubmit={handleRegister}>
-                <input type="text" 
-                placeholder="Name"
-                value={name}
-                onChange={(e)=> setName(e.target.value)}
-                required
-                />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          required
+        />
 
-                <input type="email" 
-                placeholder="Email"
-                value={email}
-                onChange={(e)=> setEmail(e.target.value)}
-                required
-                />
+        <select value={role} onChange={(e) => setRole(e.target.value)} required>
+          <option value="USER">User</option>
+          <option value="ADMIN">Admin</option>
+        </select>
 
-                <input type="password" 
-                placeholder="Password"
-                value={password}
-                onChange={(e)=> setPassword(e.target.value)}
-                required
-                />
-
-                <input type="text" 
-                placeholder="Phone Number"
-                value={phoneNumber}
-                onChange={(e)=> setPhoneNumber(e.target.value)}
-                required
-                />
-
-                <select value={role} onChange={(e) => setRole(e.target.value)} required>
-                <option value="USER">User</option>
-                <option value="ADMIN">Admin</option>
-                </select>
-
-
-                <button type="submit">Login</button>
-            </form>
-            <p>Already have an account? <a href="/login">Login</a></p>
-        </div>
-    )
-
-}
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/register">Register</Link>
+      </p>
+    </div>
+  );
+};
 
 export default RegisterPage;
